ef2134156aeaaaecfc3addbafca6726929929e5b
nebjamin <jonb1@newpaltz.edu>
2023-09-30 02:28:29 -0400

    Fix bug causing server to crash

--- server/controllers/mobs.js
+++ server/controllers/mobs.js
@@ -3,7 +3,7 @@ import { pool } from '../config/database.js';
 const getMobs = async (req, res) => {
     try {
         const results = await pool.query('SELECT * FROM mobs ORDER BY id ASC')
-        res.send(200).json(results.rows);
+        res.status(200).json(results.rows);
     } catch(error) {
         res.status(409).json( { error: error.message } )
     }
